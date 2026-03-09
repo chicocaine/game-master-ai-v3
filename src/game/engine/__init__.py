08a7410ea6aec2bd05a6162e1c1d921f9da2f5cf
chicocaine <eduardanthony1208@gmail.com>
2026-03-09 10:43:26 +0800

    feat: add InMemoryEventSink and SessionLogSink to engine exports

--- src/game/engine/__init__.py
+++ src/game/engine/__init__.py
@@ -1,6 +1,7 @@
 from game.engine.interfaces import ActionProvider, EngineContext, EventSink, Narrator, Persistence
 from game.engine.loop import EngineLoopOutcome, run_engine_loop
 from game.engine.providers import QueueActionProvider
+from game.engine.sinks import InMemoryEventSink, SessionLogSink
 
 __all__ = [
     "ActionProvider",
@@ -11,4 +12,6 @@ __all__ = [
     "EngineLoopOutcome",
     "run_engine_loop",
     "QueueActionProvider",
+    "InMemoryEventSink",
+    "SessionLogSink",
 ]
